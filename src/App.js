f607502a29784b9eea7510896e069575ac9a2503
Vipin N <vipinsuju@gmail.com>
2022-11-11 11:57:12 +0530

    sigin ui changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+
+} from "react-router-dom";
+import Homepage from './components/Homepage';
+import SignupPage from './components/Signuppage';
 
 function App() {
   return (
     <div className="App">
+
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<Homepage/>}>
+          </Route>
+          <Route path="/" exact element={<SignupPage/>}>
+          </Route>
+        </Routes>
+      </Router>
 
     </div>
   );
--- src/components/Signuppage.jsx
+++ src/components/Signuppage.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 const SignupPage = () => {
   return (
-    <div>
-      <h1>SIgnup page</h1>
+    <div className='signup-container' >
+      <section className='left-sec' >
+        <header className='header-tag' >SignIn</header>
+        <div className='input-component' >
+          <input placeholder='Username or Email' className='input-component' />
+          <input placeholder='Password' className='input-component' />
+        </div>
+        <div className='c-check' >
+          <input type="checkbox" className='check-input' />
+          <label className='label-text'>Keep me signed in</label>
+        </div>
+        <div className='bottom-btn'>
+          <button className='b-button' >Sign In</button>
+        </div>
+        <div className='bottom-elements' ></div>
+        
+      </section>
+      <section className='right-sec' >
+        <h1>right sec</h1>
+      </section>
     </div>
   )
 }
