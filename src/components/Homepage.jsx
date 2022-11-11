8834e6be4d311293c0539f4760e7187ee921188f
Vipin N <vipinsuju@gmail.com>
2022-11-11 20:49:03 +0530

    FInal changes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 const Homepage = () => {
+  const [data, setData] = useState([]);
+  const url = "https://restcountries.com/v2/all?fields=name,region,flag"
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+    }
+  };
+  console.log({ data })
+
   return (
     <div>
       <h1>Home Page</h1>
+      {data.map(item => (
+        <div className='api-main' key={item.name}>
+          <td className='name'>{item.name}</td>
+          <td className='name'>{item.region}</td>
+          <td className='image'><img style={{ width: "100px" }} src={item.flag} alt='flag' /></td>
+        </div>
+      ))}
     </div>
   )
 }
--- src/components/Signuppage.jsx
+++ src/components/Signuppage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import signInImage from "../img/Cap.PNG"
+import googleImage from "../img/g2.JPG"
+import fbImage from "../img/f2.JPG"
+import lnImage from "../img/l2.JPG"
+import tweetImage from "../img/t2.JPG"
+import { Link } from 'react-router-dom'
 
 const SignupPage = () => {
   return (
     <div className='signup-container' >
       <section className='left-sec' >
         <header className='header-tag' >SignIn</header>
-        <div className='input-component' >
+        <div className='sub-head' >
+          <span className='span-one' >New user?</span>&nbsp;
+          <span className='span-two' >Create an account</span>
+        </div>
+        <div className='input-component-main' >
           <input placeholder='Username or Email' className='input-component' />
           <input placeholder='Password' className='input-component' />
         </div>
@@ -14,13 +24,24 @@ const SignupPage = () => {
           <label className='label-text'>Keep me signed in</label>
         </div>
         <div className='bottom-btn'>
-          <button className='b-button' >Sign In</button>
+          <Link to="/home" >
+            <button className='b-button' >Sign In</button>
+          </Link>
+
         </div>
-        <div className='bottom-elements' ></div>
-        
+        <hr />
+
+        <div className='bottom-elements'>
+          <a href='https://www.google.co.in/'><img src={googleImage} alt='google' className='image-element' /></a>
+          <a href='https://www.facebook.com/'><img src={fbImage} alt='fb' className='image-element' /></a>
+          <a href='https://www.linkedin.com/'><img src={lnImage} alt='linkedIn' className='image-element' /></a>
+          <a href='https://twitter.com/tweet?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'>
+            <img src={tweetImage} alt='tweet' className='image-element' /></a>
+        </div>
+
       </section>
       <section className='right-sec' >
-        <h1>right sec</h1>
+        <img src={signInImage} alt="" />
       </section>
     </div>
   )
